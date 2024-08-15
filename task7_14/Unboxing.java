2deae3bddb1b4bec1e4c10f5439305fae531a1db
Mathumitha3 <74956844+Mathumitha3@users.noreply.github.com>
2024-08-15 23:58:05 +0530

    Sample program using wrapper class autoboxing and unboxing.

---
+++ task7_14/Unboxing.java
@@ -0,0 +1,17 @@
+package task7_14;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer i1=20;
+		int i2=i1;
+		System.out.println("Primitive int : " + i2);
+		
+		Boolean b1=true;
+		boolean b2=b1;
+		System.out.println("Primitive Boolean : " + b2);
+	}
+
+}
---
+++ task7_14/WrapperClass.java
@@ -0,0 +1,31 @@
+package task7_14;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=20;
+		Integer wrap1=a;
+//		Integer b=a;
+		System.out.println("Wrapper Integer : " + wrap1);
+		       
+		boolean b1=true;
+		Boolean wrap2=b1;
+//		Boolean bo=b1;
+		System.out.println("Wrapper Boolean : " + wrap2);
+        
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
